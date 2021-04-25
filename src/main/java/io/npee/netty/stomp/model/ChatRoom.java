8c948d0fee6295d895d3ca50fe5b8a92335b66f8
npee <jmc4664@gmail.com>
2021-04-25 18:00:40 +0900

    채팅방 빌더 추가

--- src/main/java/io/npee/netty/stomp/model/ChatRoom.java
+++ src/main/java/io/npee/netty/stomp/model/ChatRoom.java
@@ -1,9 +1,21 @@
 package io.npee.netty.stomp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
+@Builder
 public class ChatRoom {
     private String roomId;
     private String roomName;
+
+    public static ChatRoom create(String roomName) {
+        ChatRoomBuilder chatRoomBuilder = ChatRoom.builder()
+                .roomId(UUID.randomUUID().toString())
+                .roomName(roomName);
+        return chatRoomBuilder.build();
+    }
 }
