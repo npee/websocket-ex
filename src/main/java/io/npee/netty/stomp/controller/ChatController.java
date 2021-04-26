4bf0080d749e1c3aedddf60784e237801805e253
npee <jmc4664@gmail.com>
2021-04-26 21:05:29 +0900

    테스트 로그 추가

--- src/main/java/io/npee/netty/stomp/controller/ChatController.java
+++ src/main/java/io/npee/netty/stomp/controller/ChatController.java
@@ -1,25 +1,29 @@
 package io.npee.netty.stomp.controller;
 
 import io.npee.netty.stomp.model.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        log.info("payload(ChatMessage): {}", chatMessage);
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage,
-                               SimpMessageHeaderAccessor headerAccessor) {
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
