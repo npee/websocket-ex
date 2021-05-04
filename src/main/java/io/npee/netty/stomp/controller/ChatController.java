f586abc90daee20f419836d2b7891ceab3c37596
npee <jmc4664@gmail.com>
2021-05-04 23:05:59 +0900

    사용자 엔티티 CRUD 프로세스 동작 테스트

--- src/main/java/io/npee/netty/stomp/controller/ChatController.java
+++ src/main/java/io/npee/netty/stomp/controller/ChatController.java
@@ -1,6 +1,7 @@
 package io.npee.netty.stomp.controller;
 
 import io.npee.netty.stomp.model.ChatMessage;
+import io.npee.netty.stomp.model.ChatUser;
 import io.npee.netty.stomp.repository.ChatUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,9 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Slf4j
@@ -28,6 +32,12 @@ public class ChatController {
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage) {
+        ChatUser chatUser = new ChatUser();
+        chatUser.setUserId(UUID.randomUUID().toString());
+        chatUser.setName(chatMessage.getSender());
+        chatUserRepository.save(chatUser);
+        ChatUser findUser = chatUserRepository.findById(chatUser.getUserId()).get();
+        log.info("findUser: {}", findUser);
         return chatMessage;
     }
 }
