b719296756dae4037363546a636a67b39dc4bae6
npee <jmc4664@gmail.com>
2021-05-05 23:21:33 +0900

    CrudRepository 제거

--- src/main/java/io/npee/netty/stomp/repository/ChatUserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.npee.netty.stomp.repository;
-
-import io.npee.netty.stomp.model.ChatUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChatUserRepository extends CrudRepository<ChatUser, String> {
-}
