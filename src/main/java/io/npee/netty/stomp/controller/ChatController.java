27ffe1798e3411966107d756b76d10870c514117
npee <jmc4664@gmail.com>
2021-05-06 00:05:49 +0900

    CrudRepository 제거

--- src/main/java/io/npee/netty/stomp/controller/ChatController.java
+++ src/main/java/io/npee/netty/stomp/controller/ChatController.java
@@ -2,16 +2,13 @@ package io.npee.netty.stomp.controller;
 
 import io.npee.netty.stomp.model.ChatMessage;
 import io.npee.netty.stomp.model.ChatUser;
-import io.npee.netty.stomp.repository.ChatUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import java.util.Optional;
 import java.util.UUID;
 
 
@@ -19,8 +16,6 @@ import java.util.UUID;
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
-
-    private final ChatUserRepository chatUserRepository;
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
@@ -35,9 +30,6 @@ public class ChatController {
         ChatUser chatUser = new ChatUser();
         chatUser.setUserId(UUID.randomUUID().toString());
         chatUser.setName(chatMessage.getSender());
-        chatUserRepository.save(chatUser);
-        ChatUser findUser = chatUserRepository.findById(chatUser.getUserId()).get();
-        log.info("findUser: {}", findUser);
         return chatMessage;
     }
 }
--- src/main/java/io/npee/netty/stomp/model/ChatUser.java
+++ src/main/java/io/npee/netty/stomp/model/ChatUser.java
@@ -1,13 +1,9 @@
 package io.npee.netty.stomp.model;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 
 @Data
-@RedisHash("CHAT_USER")
 public class ChatUser {
-    @Id
     private String userId;
     private String name;
 }
