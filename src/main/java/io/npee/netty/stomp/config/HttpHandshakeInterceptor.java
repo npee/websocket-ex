699cb9ec26f396a7ee760b8e642b359961596dde
npee <jmc4664@gmail.com>
2021-05-14 23:58:15 +0900

    Handshake 시 HttpSession의 sessionId 얻어 header에 추가하는 인터셉터 작성

---
+++ src/main/java/io/npee/netty/stomp/config/HttpHandshakeInterceptor.java
@@ -0,0 +1,31 @@
+package io.npee.netty.stomp.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Slf4j
+public class HttpHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpSession session = servletRequest.getServletRequest().getSession();
+            String sessionId = session.getId();
+            log.info("Get/put sessionId [{}] before handshake", sessionId);
+            attributes.put("sessionId", sessionId);
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
