027adcfdc3b92d34591d8d1b14d41d8b1483d880
npee <jmc4664@gmail.com>
2021-05-19 19:01:20 +0900

    채팅 메시지 부분을 addUser 시 리스너 부착한 후 메시지 전송할땐 해당 토픽으로 발송하는 프로세스로 변경

--- src/main/java/io/npee/netty/stomp/controller/ChatController.java
+++ src/main/java/io/npee/netty/stomp/controller/ChatController.java
@@ -1,16 +1,18 @@
 package io.npee.netty.stomp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.npee.netty.stomp.listener.RedisChatPublisher;
+import io.npee.netty.stomp.listener.RedisChatSubscriber;
 import io.npee.netty.stomp.model.ChatMessage;
-import io.npee.netty.stomp.model.ChatRoom;
 import io.npee.netty.stomp.model.ChatUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-
-import java.util.UUID;
 
 
 @Slf4j
@@ -18,18 +20,24 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ChatController {
 
+    private final RedisMessageListenerContainer redisListener;
+    private final RedisChatSubscriber redisChatSubscriber;
+    private final RedisChatPublisher redisChatPublisher;
+
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+    // @SendTo("/topic/public")
+    public void sendMessage(@Payload ChatMessage chatMessage) throws JsonProcessingException {
         log.info("payload(ChatMessage): {}", chatMessage);
-        return chatMessage;
+        ChannelTopic topic = new ChannelTopic(chatMessage.getRoomId());
+        redisChatPublisher.publish(topic, chatMessage);
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage) {
         ChatUser chatUser = new ChatUser();
-        // chatUser.setUserId(UUID.randomUUID().toString());
+        ChannelTopic topic = ChannelTopic.of(chatMessage.getRoomId());
+        redisListener.addMessageListener(redisChatSubscriber, topic);
         chatUser.setSender(chatMessage.getSender());
         return chatMessage;
     }
